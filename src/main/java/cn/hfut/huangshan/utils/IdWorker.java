cfd52f1a94abc7fc0eaef60a4bdceb0f7784c3e0
pengchenyu <1106998960@qq.com>
2020-02-28 23:39:39 +0800

    Finish all complaint api

--- src/main/java/cn/hfut/huangshan/utils/IdWorker.java
+++ src/main/java/cn/hfut/huangshan/utils/IdWorker.java
@@ -159,15 +159,4 @@ public class IdWorker {
         return id;
     }
 
-
-    public static void main(String[] args) {
-
-        IdWorker idWorker=new IdWorker(0,0);
-
-        for(int i=0;i<10000;i++){
-            long nextId = idWorker.nextId();
-            System.out.println(nextId);
-        }
-    }
-
 }

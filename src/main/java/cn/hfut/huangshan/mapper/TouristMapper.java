2c6def6f41e0eeac070359c92e320fd759a8d443
pengchenyu <1106998960@qq.com>
2020-03-04 11:41:02 +0800

    Finish most of the tourist apis.

--- src/main/java/cn/hfut/huangshan/mapper/TouristMapper.java
+++ src/main/java/cn/hfut/huangshan/mapper/TouristMapper.java
@@ -1,5 +1,6 @@
 package cn.hfut.huangshan.mapper;
 
+import cn.hfut.huangshan.pojo.DB.DBTourist;
 import cn.hfut.huangshan.pojo.Tourist;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -20,4 +21,22 @@ public interface TouristMapper {
 
     //根据账号密码查询游客
     Tourist touristLogin(@Param("account") String account, @Param("password") String password);
+
+    //根据id查询
+    Tourist getById(@Param("id") long id);
+
+    //增加一名
+    Integer addOne(DBTourist dbTourist);
+
+    //更新一个
+    Integer updateOne(DBTourist dbTourist);
+
+    //修改密码
+    Integer updatePassword(@Param("id") long id, @Param("password") String encode);
+
+    //删除一个
+    Integer deleteOne(@Param("id")long id);
+
+    //修改头像
+    Integer changeHeadIcon(@Param("id")long id, @Param("headIcon")String headIconUrl);
 }
